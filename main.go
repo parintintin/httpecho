2dda200f9ad2c339016addb5aa24c5bae47df75e
Hendrik Will <hendrikwill@gmail.com>
2016-02-22 23:01:15 +0100

    Added support for Cross-origin Resource Sharing (CORS).

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 
 // httpecho is a web server which responds with the requested HTTP status code.
 func main() {
-	http.HandleFunc("/", handler)
+	finalHandler := http.HandlerFunc(handler)
+	http.Handle("/", addDefaultHeaders(finalHandler))
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
@@ -21,8 +22,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(400)
 		fmt.Fprintf(w, "No status code")
+	} else {
+		w.WriteHeader(s)
+		fmt.Fprintf(w, "Status Code: %d %s", s, http.StatusText(s))
 	}
+}
 
-	w.WriteHeader(s)
-	fmt.Fprintf(w, "Status Code: %d %s", s, http.StatusText(s))
+func addDefaultHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		next.ServeHTTP(w, r)
+	})
 }
