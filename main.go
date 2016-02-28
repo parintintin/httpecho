e77c5cca04a7345e9d7f6108828cc62d6066779b
Hendrik Will <hendrikwill@gmail.com>
2016-02-28 21:30:33 +0100

    Removed the unnecessary else branch to keep the control flow clean.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 	address string
 )
 
-// httpecho is a web server which responds with the requested HTTP status code.
+// httpecho is a HTTP server which responds with the requested HTTP status code.
 func main() {
 	flag.StringVar(&host, "host", "localhost", "Host for HTTP server")
 	flag.IntVar(&port, "port", 80, "Port to bind HTTP server to")
@@ -34,23 +34,26 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	p := splitURLPath(r.URL.Path)
 	s, err := strconv.Atoi(p[0])
-	if err == nil {
-		if d, err := strconv.Atoi(r.URL.Query().Get("d")); err == nil {
-			time.Sleep(time.Duration(d) * time.Second)
-		}
-
-		if len(p) > 1 {
-			if s == http.StatusMovedPermanently || s == http.StatusTemporaryRedirect {
-				w.Header().Add("Location", "http://"+address+"/"+p[1])
-				http.Redirect(w, r, "http://"+address+"/"+p[1], s)
-			}
-		}
-		w.WriteHeader(s)
-		fmt.Fprintf(w, "Status Code: %d %s", s, http.StatusText(s))
-	} else {
+	if err != nil {
 		w.WriteHeader(400)
-		fmt.Fprintf(w, "No status code")
+		fmt.Fprintf(w, "No status code, Error: %s", err)
 	}
+
+	// Redirect for HTTP status codes 301 and 307
+	// E.g. curl --location /301/200 is redirected to /200
+	if len(p) > 1 &&
+		s == http.StatusMovedPermanently || s == http.StatusTemporaryRedirect {
+		w.Header().Add("Location", "http://"+address+"/"+p[1])
+		http.Redirect(w, r, "http://"+address+"/"+p[1], s)
+	}
+
+	// Parameter to specify a delay for the response
+	if d, err := strconv.Atoi(r.URL.Query().Get("d")); err == nil {
+		time.Sleep(time.Duration(d) * time.Second)
+	}
+
+	w.WriteHeader(s)
+	fmt.Fprintf(w, "Status Code: %d %s", s, http.StatusText(s))
 }
 
 func addDefaultHeaders(next http.Handler) http.Handler {
